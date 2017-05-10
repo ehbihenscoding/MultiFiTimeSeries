8d33d46c59b40b61d9b900b01e9495861cc98c52
ehbihenscoding <baptiste.kerleguer@polytechnique.edu>
2017-05-10 10:28:15 +0200

    Modifications of the outputs

--- MakeExp.R
+++ MakeExp.R
@@ -25,11 +25,11 @@ for( experi in 1:Nb_exper){
 	Q2SVD1Fstat[,experi] = Q2iteration
 }
 
-Q2SVD2FLFmean <- apply( Q2SVD2FLFstat[], 1, mean)
-Q2SVD2FHFmean <- apply( Q2SVD2FHFstat[], 1, mean)
-Q2TENCOV2Fmean	<- apply( Q2TENCOV2Fstat[], 1, mean)
-Q2SVD1Fmean	<- apply( Q2SVD1Fstat[], 1, mean)
+Q2SVD2FLFmean <- apply( Q2SVD2FLFstat, 1, mean)
+Q2SVD2FHFmean <- apply( Q2SVD2FHFstat, 1, mean)
+Q2TENCOV2Fmean	<- apply( Q2TENCOV2Fstat, 1, mean)
+Q2SVD1Fmean	<- apply( Q2SVD1Fstat, 1, mean)
 
 #### Export
-data = data.frame( t=t, Q2SVD2FLFmean=Q2SVD2FLFmean, Q2SVD2FHFmean=Q2SVD2FHFmean, Q2SVD1Fmean=Q2SVD1Fmean, Q2TENCOV2Fmean=Q2TENCOV2Fmean)
+data = data.frame( t=t, Q2SVD2FLFmean=Q2SVD2FLFstat, Q2SVD2FHFmean=Q2SVD2FHFstat, Q2SVD1Fmean=Q2SVD1Fstat, Q2TENCOV2Fmean=Q2TENCOV2Fstat)
 write.csv(data, "~/outputs/exportQ2.csv")
