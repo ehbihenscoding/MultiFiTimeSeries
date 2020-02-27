449efa7e0e4645ea9c11df187371ef6e02deab39
ehbihenscoding <baptiste.kerleguer@ens-paris-saclay.fr>
2020-02-27 20:34:53 +0100

    Solving names problems of Q2

--- MakeExp.R
+++ MakeExp.R
@@ -1,37 +1,29 @@
-setwd('~/Documents/These/Code/Signaux/Decomposition/Mean/optim/')
+#setwd('~/Documents/These/Code/Signaux/Decomposition/Mean/optim/')
 #setwd('~/Rcode/optim/')
-#setwd('~/ThËse/Travail/codage/MultifideliteTemporel/optim/')
+setwd('~/ThËse/Travail/codage/MultifideliteTemporel/optim/')
 source('example/Import.R')
 
 # d√©finition des param√®tres de l'exp√©rience
-Nb_exper = 40
+Nb_exper = 4
 Nt = 101
-# Initialisation parallelisation
-lock <- tempfile()
 # creations des matrices de Q2
 Q2SVD2FLFstat = matrix( 0, Nt, Nb_exper)
 Q2SVD2FHFstat = matrix( 0, Nt, Nb_exper)
 Q2TENCOV2Fstat = matrix( 0, Nt, Nb_exper)
 Q2SVD1Fstat = matrix( 0, Nt, Nb_exper)
 
-##### Parallelisation #######
-registerDoSEQ()
-c1 <- parallel::makeCluster(12)
-doParallel::registerDoParallel(c1)
-
 for( experi in 1:Nb_exper){
 	source('example/Import.R')
 	source('example/data.R')
 	source('SVD2FLF/SVD2FLF.R')
-	Q2SVD2FLFstat[,experi] = Q2SVD2FLFoptim
+	Q2SVD2FLFstat[,experi] = Q2valSVD2FLF
 	source('SVD2FHF/SVD2FHF.R')
-	Q2SVD2FHFstat[,experi] = Q2SVD2FHFoptim
+	Q2SVD2FHFstat[,experi] = Q2valSVD2FHF
 	source('TENSVD2F/TENSVD2F.R')
-	Q2TENCOV2Fstat[,experi] = Q2TENSVD2Foptim
+	Q2TENCOV2Fstat[,experi] = Q2valTENSVD2F
 	source('SVD1F/SVD1F.r')
-	Q2SVD1Fstat[,experi] = Q2iteration
+	Q2SVD1Fstat[,experi] = Q2valSVD1F
 }
-###### Fin Parallelisation  #######
 
 Q2SVD2FLFmean <- apply( Q2SVD2FLFstat[], 1, mean)
 Q2SVD2FHFmean <- apply( Q2SVD2FHFstat[], 1, mean)
