265825e1e5784fb55d623e2d24f66d5e2cddb4b4
ehbihenscoding <baptiste.kerleguer@polytechnique.edu>
2020-02-28 10:36:12 +0100

    Addition of the dimension

--- MakeExp.R
+++ MakeExp.R
@@ -1,6 +1,6 @@
-#setwd('~/Documents/These/Code/Signaux/Decomposition/Mean/optim/')
+setwd('~/Documents/These/Code/Signaux/Decomposition/Mean/optim/')
 #setwd('~/Rcode/optim/')
-setwd('~/ThËse/Travail/codage/MultifideliteTemporel/optim/')
+#setwd('~/ThËse/Travail/codage/MultifideliteTemporel/optim/')
 source('example/Import.R')
 
 # d√©finition des param√®tres de l'exp√©rience
@@ -11,16 +11,22 @@ Q2SVD2FLFstat = matrix( 0, Nt, Nb_exper)
 Q2SVD2FHFstat = matrix( 0, Nt, Nb_exper)
 Q2TENCOV2Fstat = matrix( 0, Nt, Nb_exper)
 Q2SVD1Fstat = matrix( 0, Nt, Nb_exper)
+dimBaseTENCOV = rep( 0, Nb_exper)
+dimBaseSVD2LF = rep( 0, Nb_exper)
+dimBaseSVD2HF = rep( 0, Nb_exper)
 
 for( experi in 1:Nb_exper){
 	source('example/Import.R')
 	source('example/data.R')
 	source('SVD2FLF/SVD2FLF.R')
 	Q2SVD2FLFstat[,experi] = Q2valSVD2FLF
+	dimBaseSVD2LF[experi] = nb_optim
 	source('SVD2FHF/SVD2FHF.R')
 	Q2SVD2FHFstat[,experi] = Q2valSVD2FHF
+	dimBaseSVD2HF[experi] = nb_optim
 	source('TENSVD2F/TENSVD2F.R')
 	Q2TENCOV2Fstat[,experi] = Q2valTENSVD2F
+	dimBaseTENCOV[experi] = nb_optimTENCOV 
 	source('SVD1F/SVD1F.r')
 	Q2SVD1Fstat[,experi] = Q2valSVD1F
 }
