4c421ef58c576ac6cff672c354c4896092a14de3
ehbihenscoding <baptiste.kerleguer@ens-paris-saclay.fr>
2020-05-03 09:01:04 +0200

    Add of Var comp

---
+++ Plots/VarCompAll.r
@@ -0,0 +1,10 @@
+# Comparing the computations of the variance
+
+x11(); #new window
+plot( t, a[,i], type='l', col=2)    #true curve
+#TENSVD2F model
+lines( t, pred[,i], col=1) 
+lines( t, pred[,i] + 1.96*sqrt(pvar[,i]), col=3, lty=2)
+lines( t, pred[,i] - 1.96*sqrt(pvar[,i]), col=3, lty=2)
+lines( t, pred[,i] + 1.96*sqrt(pvaralter[,i]), col=4, lty=2)
+lines( t, pred[,i] - 1.96*sqrt(pvaralter[,i]), col=4, lty=2)
