f1b8949727d78eff2593fdeac77e1024eca155e7
ehbihenscoding <baptiste.kerleguer@ens-paris-saclay.fr>
2020-05-03 07:35:31 +0200

    Add spaces

--- Plots/Q2plot.r
+++ Plots/Q2plot.r
@@ -1,6 +1,6 @@
 ##### Affichage Q2 #####
 x11();  #new figure
-plot(t,Q2SVD2FHFmean,type='l',ylim=c(0.8,1))    #Q2SVD2FHF
-lines(t,Q2TENCOV2Fmean, col=2)  #Q2TENCOV2F
-lines(t,Q2SVD2FLFmean, col=3)   #Q2SVD2FLF
+plot( t, Q2SVD2FHFmean, type='l', ylim=c(0.8,1))    #Q2SVD2FHF
+lines( t, Q2TENCOV2Fmean, col=2)  #Q2TENCOV2F
+lines( t, Q2SVD2FLFmean, col=3)   #Q2SVD2FLF
 lines( t, Q2SVD1Fmean, col=4)   #Q2SVD1F
