09e887f99119a05bc61c7d514da649ae62328702
ehbihenscoding <baptiste.kerleguer@ens-paris-saclay.fr>
2020-05-03 07:18:54 +0200

    Replace digit by i

--- Plots/TestCurveMeanVar.r
+++ Plots/TestCurveMeanVar.r
@@ -5,8 +5,8 @@ x11();  #create a new window
 plot( t, a[,i], type='l', col=2)    #true curve
 #TENSVD2F model
 lines( t, pred[,i], col=1) 
-lines( t, pred[,i] + 1.96*sqrt(pvar[,1]), col=1, lty=2)
-lines( t, pred[,i] - 1.96*sqrt(pvar[,1]), col=1, lty=2)
+lines( t, pred[,i] + 1.96*sqrt(pvar[,i]), col=1, lty=2)
+lines( t, pred[,i] - 1.96*sqrt(pvar[,i]), col=1, lty=2)
 #SVD2FHF model
 lines( t, pmean[,i], type='l', col=3)
 lines( t, pmean[,i]+1.96*sqrt(varpredHF[,i]), col=3, lty=2)
