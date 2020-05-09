6dcfd359672a48c36b6fb7a77bfb8c94bd376bfa
ehbihenscoding <baptiste.kerleguer@ens-paris-saclay.fr>
2020-05-09 20:47:22 +0200

    Add of polygon for unsertenty zone

--- Plots/TestCurveMeanVar.r
+++ Plots/TestCurveMeanVar.r
@@ -5,13 +5,20 @@ x11();  #create a new window
 plot( t, a[,i], type='l', col=2)    #true curve
 #TENSVD2F model
 lines( t, pred[,i], col=1) 
-lines( t, pred[,i] + 1.96*sqrt(pvar[,i]), col=1, lty=2)
-lines( t, pred[,i] - 1.96*sqrt(pvar[,i]), col=1, lty=2)
-#SVD2FHF model
-lines( t, pmeanHF[,i], type='l', col=3)
-lines( t, pmeanHF[,i]+1.96*sqrt(varpredHF[,i]), col=3, lty=2)
-lines( t, pmeanHF[,i]-1.96*sqrt(varpredHF[,i]), col=3, lty=2)
-#SVD2FLF model
-lines( t, pmeanLF[,i], type='l', col=4)
-lines( t, pmeanLF[,i]+1.96*sqrt(varpredLF[,i]), col=4, lty=2)
-lines( t, pmeanLF[,i]-1.96*sqrt(varpredLF[,i]), col=4, lty=2)
+lines( t, pred[,i] + 1.96*sqrt(pvarformulefull[,i]), col=4, lty=2)
+lines( t, pred[,i] - 1.96*sqrt(pvarformulefull[,i]), col=4, lty=2)
+polygon( c( t, t[length(t):1]), c( pred[,i] + 1.96*sqrt(pvarformulefull[,i]), (pred[,i] - 1.96*sqrt(pvarformulefull[,i]))[length(pred[,i]):1]),
+	col =  4, border = NA, density = 4, lty= "dashed")
+polygon( c( t, t[length(t):1]), c( pred[,i] + 1.96*sqrt(pvaralter[,i]), (pred[,i] - 1.96*sqrt(pvaralter[,i]))[length(pred[,i]):1]),
+	col =  3, border = NA, density = 4, lty= "dashed")
+lines( t, pred[,i] + 1.96*sqrt(pvaralter[,i]), col=3, lty=2)
+lines( t, pred[,i] - 1.96*sqrt(pvaralter[,i]), col=3, lty=2)
+
+##SVD2FHF model
+#lines( t, pmeanHF[,i], type='l', col=3)
+#lines( t, pmeanHF[,i]+1.96*sqrt(varpredHF[,i]), col=3, lty=2)
+#lines( t, pmeanHF[,i]-1.96*sqrt(varpredHF[,i]), col=3, lty=2)
+##SVD2FLF model
+#lines( t, pmeanLF[,i], type='l', col=4)
+#lines( t, pmeanLF[,i]+1.96*sqrt(varpredLF[,i]), col=4, lty=2)
+#lines( t, pmeanLF[,i]-1.96*sqrt(varpredLF[,i]), col=4, lty=2)
