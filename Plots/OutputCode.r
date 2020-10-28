8db469c0e38af33526af313eefac9a2811027418
ehbihenscoding <baptiste.kerleguer@polytechnique.edu>
2020-10-28 14:33:01 +0100

    Comments for plots

--- Plots/OutputCode.r
+++ Plots/OutputCode.r
@@ -3,8 +3,9 @@ setofData = Z1
 
 #x11();
 
-pdf("lowFiData.pdf")
+#pdf("Data.pdf")
 plot( t, setofData[,N2], type='l', ylim=c(min(setofData),max(setofData)),
-        ylab="Zh(x,t)")
-claire( t, setofData[,(N1-N2):N1], N2)
-dev.off()
+        ylab="Zh(x,t)",col=rgb(0,0,1,0.3))
+claire( t, setofData, N1, col=rgb(0,0,1,0.3))
+claire( t, Z2, N2, col=2)
+#dev.off()
